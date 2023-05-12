821ec418f945360c6d456ba7f1490f52d10e7ad0
expeditus303 <ricardotonietto@gmail.com>
2023-05-12 19:39:07 -0300

    fix: create customer schema

--- src/schemas/customers.schemas.js
+++ src/schemas/customers.schemas.js
@@ -4,7 +4,9 @@ import joi from "joi"
 const create = joi.object({
         name: joi.string().max(120).required(),
         phone: joi.string().min(10).max(11).required(),
-        cpf: joi.string().length(11).required(), 
+        cpf: joi.string().pattern(/^[0-9]+$/).length(11).required().messages({
+            'string.pattern.base': '\"cpf\" must contain only numbers',
+          }), 
         birthday: joi.date().required()
 })
 
